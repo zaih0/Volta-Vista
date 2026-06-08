6cfaa35dc86bc010bb0e2a83f993ea8c53ca7afa
Zaine Horn <113855619+zaih0@users.noreply.github.com>
2026-06-08 13:08:55 +0200

    button links

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -32,4 +32,4 @@ func _on_restart_button_pressed():
 	get_tree().reload_current_scene()
 
 func _on_quit_button_pressed():
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://scenes/home_screen.tscn")
