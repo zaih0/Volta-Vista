b5b22c59fffb16c9dcd29d8d55ca8d568319fb6b
Zaine Horn <113855619+zaih0@users.noreply.github.com>
2026-06-08 12:45:47 +0200

    ui pause menu

---
+++ scenes/pause_menu.gd
@@ -0,0 +1,35 @@
+extends Control
+
+@onready var resume_button = $CenterContainer/Panel/MarginContainer/VBoxContainer/ResumeButton
+@onready var restart_button = $CenterContainer/Panel/MarginContainer/VBoxContainer/RestartButton
+@onready var quit_button = $CenterContainer/Panel/MarginContainer/VBoxContainer/QuitButton
+
+func _ready():
+	visible = false
+
+	resume_button.pressed.connect(_on_resume_button_pressed)
+	restart_button.pressed.connect(_on_restart_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		toggle_pause()
+
+func toggle_pause():
+	if get_tree().paused:
+		get_tree().paused = false
+		visible = false
+	else:
+		get_tree().paused = true
+		visible = true
+
+func _on_resume_button_pressed():
+	get_tree().paused = false
+	visible = false
+
+func _on_restart_button_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+func _on_quit_button_pressed():
+	get_tree().quit()
